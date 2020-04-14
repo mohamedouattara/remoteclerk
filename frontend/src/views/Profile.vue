5bb4c2f9bf2226db02cdf2323529be5299d05e69
AdrianKrebs <kra@panter.ch>
2020-04-14 15:12:51 +0200

    small improvements

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -30,7 +30,7 @@
                 <b-col>
                     <h2 class="hero-title mt-0 is-revealing">Activation</h2>
                     <p>Activate or deactivate the widget on your site. We will add a feature for displaying the widget during your
-                    opening hours soon.</p>
+                        opening hours soon.</p>
                     <b>Widget active : {{ company.widgetActive }}</b>
                     <b-form-checkbox :checked="company.widgetActive" name="check-button" @change="activationChanged" switch>
                     </b-form-checkbox>
@@ -45,68 +45,15 @@
                         {{getCallUrl}}
                     </code>
                     <h3>Widget</h3>
-                    <p>You could also add our call widget to your website. Just copy the following code to your website and you are
+                    <p>You could also add our video consulting widget to your website. Just include the remoteclerk JS library file in your
+                        HTML head element and you are
                         ready to go. The sample widget is displayed at the bottom right of this page.</p>
 
                     <figure>
-                    <pre>
-                    <code class="code">
-                    &lt;html&gt;
-                        &lt;div class="remoteclerk-popup"&gt;
-                            &lt;button class="video-call-button"&gt;
-                                &lt;p class="live-badge"&gt;LIVE&lt;/p&gt;
-                                &lt;div class="video-icon"&gt;
-                                    &lt;svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="video"
-                                         class="svg-inline--fa fa-video fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg"
-                                         viewBox="0 0 576 512"&gt;
-                                        &lt;path fill="currentColor"
-                                              d="M336.2 64H47.8C21.4 64 0 85.4 0 111.8v288.4C0 426.6 21.4 448 47.8 448h288.4c26.4 0 47.8-21.4 47.8-47.8V111.8c0-26.4-21.4-47.8-47.8-47.8zm189.4 37.7L416 177.3v157.4l109.6 75.5c21.2 14.6 50.4-.3 50.4-25.8V127.5c0-25.4-29.1-40.4-50.4-25.8z"&gt;&lt;/path&gt;
-                                    &lt;/svg&gt;
-                                &lt;/div&gt;
-                                Video Consultation
-                            &lt;/button&gt;
-                        &lt;/div&gt;
-                    &lt;/html&gt;
-                    &lt;style&gt;
-                        .remoteclerk-popup {
-                            background-color: white;
-                            border: 1px solid #1d1d1b;
-                            border-radius: 4px;
-                            padding: 10px;
-                            position: fixed;
-                            bottom: 10px;
-                            right: 10px;
-                            transition: all .5s ease-in-out;
-                            transform-origin: right bottom;
-                            z-index: 101;
-                            font-size: 12px;
-
-                        .video-call-button {
-                            outline: none;
-                            border: none;
-                            background: white;
-                        }
-
-                        .live-badge {
-                            background: red;
-                            color: white;
-                            padding-bottom: 0;
-                            margin-bottom: 0;
-                        }
-
-                        .video-icon {
-                            display: flex;
-                            justify-content: center;
-                            align-items: center;
-
-                        svg {
-                            height: 50px
-                        }
-                        }
-                        }
-                    &lt;/style&gt;
-                    </code>
-                </pre>
+                        <code class="code">
+                            &lt;script src="https://remoteclerk.com/widget/"&gt;
+                            &lt;/script&gt;
+                        </code>
                     </figure>
                     <div class="remoteclerk-popup">
                         <button class="video-call-button" @click="openCallPage()">
